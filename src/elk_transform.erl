59619995a8d9aa3f7e7e31399d57bbed1914d467
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-05-26 13:50:35 +0300

    compound block and inverse tags, write some tests

--- src/elk_transform.erl
+++ src/elk_transform.erl
@@ -12,6 +12,34 @@ prefix_key_postfix(Kind, Node) ->
 	Postfix = ?iol2b(?get(postfix, Node)),
 	{Kind, Key, Prefix, Postfix}.
 
+%% find matching pairs for blocks
+block_construct([{block_start, Key, Prefix, Postfix} | Nodes], Expected, Acc) ->
+	block_construct(Nodes, [ {block_start, Key, Prefix, Postfix} | Expected], Acc);
+block_construct([{inverse_start, Key, Prefix, Postfix} | Nodes], Expected, Acc) ->
+	block_construct(Nodes, [ {inverse_start, Key, Prefix, Postfix} | Expected], Acc);
+block_construct(
+	[{block_end, Key, EndPrefix, EndPostfix} | Nodes],
+	[{block_start, Key, StartPrefix, StartPostfix} | Expected],
+	Acc
+) ->
+	SubTemplate = block_construct(lists:reverse(Acc), [], []),
+	Rest = block_construct(Nodes, Expected, []),
+	Node = {block, Key, [{StartPrefix, StartPostfix}, {EndPrefix, EndPostfix}], SubTemplate},
+	[Node | Rest];
+block_construct(
+	[{block_end, Key, EndPrefix, EndPostfix} | Nodes],
+	[{inverse_start, Key, StartPrefix, StartPostfix} | Expected],
+	Acc
+) ->
+	SubTemplate = block_construct(lists:reverse(Acc), [], []),
+	Rest = block_construct(Nodes, Expected, []),
+	Node = {inverse, Key, [{StartPrefix, StartPostfix}, {EndPrefix, EndPostfix}], SubTemplate},
+	[Node | Rest];
+block_construct([Node | Nodes], Expected, Acc) ->
+	block_construct(Nodes, Expected, [Node | Acc]);
+block_construct([], [], Acc) ->
+	lists:reverse(Acc).
+
 transform(text, Node, _Index) -> {text, ?iol2b(Node)};
 
 transform(var,           Node, _Index) -> prefix_key_postfix(var,           Node);
@@ -20,6 +48,9 @@ transform(var_escaped2,  Node, _Index) -> prefix_key_postfix(escaped,       Node
 transform(block_start,   Node, _Index) -> prefix_key_postfix(block_start,   Node);
 transform(inverse_start, Node, _Index) -> prefix_key_postfix(inverse_start, Node);
 transform(block_end,     Node, _Index) -> prefix_key_postfix(block_end,     Node);
+
+transform(template, Node, _Index) ->
+	block_construct(Node, [], []);
 
 transform(_Symbol, Node, _Index) ->
 	Node.
--- test/parser_test.erl
+++ test/parser_test.erl
@@ -9,3 +9,18 @@ basic_test() ->
 	?assertEqual(
 		[{text, <<"aaa">>}, {var, <<"key">>, <<>>, <<>>}, {text, <<"bbb">>}],
 		elk_parser:parse("aaa{{key}}bbb")).
+
+block_test() ->
+	?assertEqual(
+		[{block, <<"key">>, [{<<>>, <<>>}, {<<>>, <<>>}], [{text, <<" text ">>}]}],
+		elk_parser:parse("{{# key }} text {{/ key }}")).
+
+inverse_test() ->
+	?assertEqual(
+		[{inverse, <<"key">>, [{<<>>, <<>>}, {<<>>, <<>>}], [{text, <<" text ">>}]}],
+		elk_parser:parse("{{^ key }} text {{/ key }}")).
+
+standalone_block_tag_test() ->
+	?assertEqual(
+		[{inverse, <<"key">>, [{<<"\r\n ">>, <<" \n">>}, {<<>>, <<" \n">>}], [{text, <<" text ">>}]}],
+		elk_parser:parse("\r\n {{^ key }} \n text {{/ key }} \n")).
