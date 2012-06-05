c8ca242c8d077008d7e5d245174d84955ab1e1f7
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-06-05 20:28:27 +0300

    fix parser tests

--- test/parser_test.erl
+++ test/parser_test.erl
@@ -7,25 +7,25 @@
 
 basic_test() ->
 	?assertEqual(
-		[indent, [{text, <<"aaa">>}, {var, [<<"key">>]}, {text, <<"bbb">>}]],
+		[indent, [{text, <<"aaa">>}, {var, [<<"key">>]}, {text, <<"bbb">>}], eof],
 		elk_parser:parse("aaa{{key}}bbb")).
 
 comment_test() ->
 	?assertEqual(
-		[indent, [{ws, <<" ">>}, comment, {ws, <<" ">>}], {nl, <<"\r\n">>}, indent],
+		[indent, [{ws, <<" ">>}, comment, {ws, <<" ">>}], {nl, <<"\r\n">>}, indent, eof],
 		elk_parser:parse(" {{! ololo! \r\n\n huh! %^&*() }} \r\n")).
 
 block_test() ->
 	?assertEqual(
 		[indent,
 			[{block, [<<"key">>],
-				[[{text, <<" text1 ">>}],{nl, <<"\n">>}, indent],
-				[{text, <<" text2 ">>}]}]],
+				[[{text, <<" text1 ">>}],{nl, <<"\n">>}],
+				[indent, [{text, <<" text2 ">>}]]}], eof],
 		elk_parser:parse("{{# key }} text1 \n text2 {{/ key }}")).
 
 inverse_test() ->
 	?assertEqual(
-		[indent, [{inverse, [<<"key">>], [], [{text, <<" text ">>}]}]],
+		[indent, [{inverse, [<<"key">>], [], [[{text, <<" text ">>}]]}], eof],
 		elk_parser:parse("{{^ key }} text {{/ key }}")).
 
 standalone_block_tag_test() ->
@@ -33,10 +33,10 @@ standalone_block_tag_test() ->
 		[indent, {nl, <<"\r\n">>}, indent,
 			[{ws, <<" ">>},
 			{inverse, [<<"key">>],
-				[[{ws, <<" ">>}], {nl, <<"\n">>}, indent],
-				[{text, <<" text ">>}]},
+				[[{ws, <<" ">>}], {nl, <<"\n">>}],
+				[indent, [{text, <<" text ">>}]]},
 			{ws, <<" ">>}],
-		{nl, <<"\n">>}, indent],
+		{nl, <<"\n">>}, indent, eof],
 		elk_parser:parse("\r\n {{^ key }} \n text {{/ key }} \n")).
 
 nested_blocks_test() ->
@@ -44,6 +44,6 @@ nested_blocks_test() ->
 		[indent,
 			[{text, <<" 1 ">>},
 			{block, [<<"first">>], [],
-				[{text, <<" 2 ">>}, {inverse, [<<"second">>], [], [{text, <<" 3 ">>}]}, {text, <<" 4 ">>}]},
-			{text, <<" 5 ">>}]],
+				[[{text, <<" 2 ">>}, {inverse, [<<"second">>], [], [[{text, <<" 3 ">>}]]}, {text, <<" 4 ">>}]]},
+			{text, <<" 5 ">>}], eof],
 		elk_parser:parse(" 1 {{#first}} 2 {{^second}} 3 {{/second}} 4 {{/first}} 5 ")).
