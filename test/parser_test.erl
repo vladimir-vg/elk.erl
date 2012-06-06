e06dc25c3b3ba5f04486c0ab953a4a5bd593ce58
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-06-07 01:40:32 +0300

    add tests for broken templates compilation

--- test/parser_test.erl
+++ test/parser_test.erl
@@ -47,3 +47,10 @@ nested_blocks_test() ->
 				[[{text, <<" 2 ">>}, {inverse, [<<"second">>], [], [[{text, <<" 3 ">>}]]}, {text, <<" 4 ">>}]]},
 			{text, <<" 5 ">>}], eof],
 		elk_parser:parse(" 1 {{#first}} 2 {{^second}} 3 {{/second}} 4 {{/first}} 5 ")).
+
+unmatched_block_test() ->
+	?assertMatch({error, _}, elk:compile("{{#block}}")),
+	?assertMatch({error, _}, elk:compile("{{/block}}")).
+
+unbalanced_block_test() ->
+	?assertMatch({error, _}, elk:compile("{{#one}}{{#two}}{{/one}}{{/two}}")).
